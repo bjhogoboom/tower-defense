be4451a3f4ccc334b6331f477ce7945bce6e31f6
Ben Hogoboom <bjhogoboom@gmail.com>
2025-03-31 21:50:30 -0700

    Change Detection

---
+++ enemy.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@export var speed: float = 30.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	position += Vector2.RIGHT * speed * delta
