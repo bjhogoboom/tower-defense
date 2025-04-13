3d9e3bce2a8beb44f7f6c2fc3271ecac08e2a04a
Ben Hogoboom <bjhogoboom@gmail.com>
2025-04-12 20:37:44 -0700

    Add enemy tracking

--- enemy.gd
+++ enemy.gd
@@ -1,3 +1,4 @@
+class_name Enemy
 extends Node2D
 
 @export var speed: float = 30.0
--- turret.gd
+++ turret.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var focused_enemy: Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,7 +10,17 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func _physics_process(delta: float) -> void:
+	if focused_enemy:
+		point_at(focused_enemy.position)
 
+func _on_focus_area_area_exited(area: Area2D) -> void:
+	focused_enemy = null
 
 func _on_focus_area_area_entered(area: Area2D) -> void:
+	focused_enemy = area
 	print("Hey there" + str(area))
+
+func point_at(point: Vector2):
+	rotation = point.angle_to_point(position) + Vector2.UP.angle()
