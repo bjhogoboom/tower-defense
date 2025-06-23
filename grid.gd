4f8d7f15377a334f8ecd9fcbb8d844fc47e92f85
Ben Hogoboom <bjhogoboom@gmail.com>
2025-06-22 22:17:36 -0700

    Cost system

--- grid.gd
+++ grid.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 var turret_scene = preload("res://turret.tscn")
 var selected_turret: Turret
+var turret_cost: int = 25
+@onready var coin_purse: CoinPurse = $CoinPurse
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,6 +15,7 @@ func _process(delta: float) -> void:
 
 func _unhandled_input(event: InputEvent) -> void:
 	if should_place_turret(event):
+		coin_purse.remove_gold(turret_cost)
 		selected_turret.activate()
 		selected_turret = null
 
@@ -20,7 +23,8 @@ func should_place_turret(event: InputEvent) -> bool:
 	return event is InputEventMouseButton \
 		and event.is_pressed() \
 		and event.button_index == MOUSE_BUTTON_LEFT \
-		and selected_turret != null
+		and selected_turret != null \
+		and coin_purse.gold >= turret_cost
 
 func _on_ui_layer_turret_selected() -> void:
 	selected_turret = turret_scene.instantiate()
