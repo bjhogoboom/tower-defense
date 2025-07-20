c233b172d91c98b57b2f5bd0d2b1cf0bdea9e8c5
Ben Hogoboom <bjhogoboom@gmail.com>
2025-07-20 11:58:55 -0700

    Refactor FocusArea into its own scene using the new Indicator

---
+++ focus_area.gd
@@ -0,0 +1,56 @@
+class_name FocusArea
+extends Area2D
+
+var focus_queue: Array[Node2D]
+var mode: Mode = Mode.ACTIVE
+
+enum Mode {PREVIEW, ACTIVE}
+
+signal added_to_queue(enemy: Enemy)
+signal removed_from_queue(enemy: Enemy)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+func focused_enemy() -> Node2D:
+	if focus_queue.is_empty(): return null
+	return focus_queue.front()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func _physics_process(delta: float) -> void:
+	pass
+
+func _on_area_exited(area: Area2D) -> void:
+	remove_from_focus_queue(area)
+	#fire_rate.stop()
+
+func _on_area_entered(area: Area2D) -> void:
+	if !mode == Mode.ACTIVE: return
+	add_to_focus_queue(area)
+
+func add_to_focus_queue(area: Area2D) -> void:
+	if area is Enemy:
+		added_to_queue.emit(area)
+		area.died.connect(_on_enemy_died)
+	focus_queue.push_back(area)
+
+func preview():
+	mode = Mode.PREVIEW
+
+func activate():
+	var enemies_in_area = get_overlapping_areas()
+	for enemy in enemies_in_area:
+		add_to_focus_queue(enemy)
+	mode = Mode.ACTIVE
+
+func remove_from_focus_queue(enemy: Node2D):
+	focus_queue.erase(enemy)
+	removed_from_queue.emit(enemy)
+
+func _on_enemy_died(enemy: Node2D):
+	remove_from_focus_queue(enemy)
+	
--- turret.gd
+++ turret.gd
@@ -1,11 +1,10 @@
 class_name Turret
 extends Node2D
 
-var focus_queue: Array[Node2D]
 var bullet_scene = preload("res://bullet.tscn")
 @onready var fire_rate: Timer = $FireRate
 @onready var spawn_point = $SpawnPoint
-@onready var focus_area: Area2D = $FocusArea
+@onready var focus_area: FocusArea = $FocusArea
 @export var damage = 1.0
 var mode: Mode = Mode.ACTIVE
 
@@ -15,10 +14,6 @@ enum Mode {PREVIEW, ACTIVE}
 func _ready() -> void:
 	pass
 
-func focused_enemy() -> Node2D:
-	if focus_queue.is_empty(): return null
-	return focus_queue.front()
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if mode == Mode.PREVIEW:
@@ -27,26 +22,8 @@ func _process(delta: float) -> void:
 func _physics_process(delta: float) -> void:
 	if !mode == Mode.ACTIVE: return
 
-	if focused_enemy():
-		point_at(focused_enemy().global_position)
-
-func _on_focus_area_area_exited(area: Area2D) -> void:
-	remove_from_focus_queue(area)
-	fire_rate.stop()
-
-func _on_focus_area_area_entered(area: Area2D) -> void:
-	if !mode == Mode.ACTIVE: return
-	add_to_focus_queue(area)
-
-func add_to_focus_queue(area: Area2D) -> void:
-	if area is Enemy:
-		area.died.connect(_on_enemy_died)
-	focus_queue.push_back(area)
-	fire_rate.start()
-
-func _on_fire_rate_timeout() -> void:
-	if focused_enemy():
-		shoot()
+	if focus_area.focused_enemy():
+		point_at(focus_area.focused_enemy().global_position)
 
 func point_at(point: Vector2):
 	rotation = point.angle_to_point(position) + Vector2.UP.angle()
@@ -60,19 +37,20 @@ func shoot():
 	get_parent().add_child(bullet)
 
 func preview():
+	focus_area.preview()
 	mode = Mode.PREVIEW
 
 func activate():
-	var enemies_in_area = focus_area.get_overlapping_areas()
-	for enemy in enemies_in_area:
-		add_to_focus_queue(enemy)
+	focus_area.activate()
 	mode = Mode.ACTIVE
 
-func remove_from_focus_queue(enemy: Node2D):
-	focus_queue.erase(enemy)
-	if focus_queue.is_empty():
+func _on_focus_area_removed_from_queue(enemy: Enemy):
+	if focus_area.focus_queue.is_empty():
 		fire_rate.stop()
-	
-func _on_enemy_died(enemy: Node2D):
-	remove_from_focus_queue(enemy)
-	
+		
+func _on_focus_area_added_to_queue(enemy: Enemy) -> void:
+	fire_rate.start()
+
+func _on_fire_rate_timeout() -> void:
+	if focus_area.focused_enemy():
+		shoot()
